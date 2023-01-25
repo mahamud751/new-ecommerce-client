60e008cafa7ceaa17e52a1573d5d919119ac4d54
mahamud751 <55927923+mahamud751@users.noreply.github.com>
2023-01-26 03:26:59 +0600

    added

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+
+import useScript from "../../components/Common/Reload";
 import {
   add_item,
   delete_cart,
